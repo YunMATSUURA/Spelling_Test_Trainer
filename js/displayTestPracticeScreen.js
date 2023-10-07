fc7874fb75280c4c507a536dafb3c44d85a6737e
yunmatsuura <matsuyui@hotmail.co.jp>
2023-10-07 13:51:16 -0500

    change to case not sensitive. change from keyCode to key to receive key input

--- js/displayTestPracticeScreen.js
+++ js/displayTestPracticeScreen.js
@@ -146,7 +146,7 @@
       
             if (playingBoolean){
         
-              if(65<=e.keyCode && e.keyCode<=90){
+              if(e.key==='a'||e.key==='b'||e.key==='c'||e.key==='d'||e.key==='e'||e.key==='f'||e.key==='g'||e.key==='h'||e.key==='i'||e.key==='j'||e.key==='k'||e.key==='l'||e.key==='m'||e.key==='n'||e.key==='o'||e.key==='p'||e.key==='q'||e.key==='r'||e.key==='s'||e.key==='t'||e.key==='u'||e.key==='v'||e.key==='w'||e.key==='x'||e.key==='y'||e.key==='z'){
                 inputWord += e.key;
                 userInput.textContent = inputWord;
               } else if (e.key==='Delete'){
@@ -155,7 +155,7 @@
               } else if(e.key==='Backspace'){
                 inputWord = inputWord.slice(0,inputWord.length-1)
                 userInput.textContent = inputWord;
-              } else if (e.key==='Shift'){
+              } else if (e.shiftKey){
                 e.preventDefault       
               } else if (e.key==='Enter'){
       
